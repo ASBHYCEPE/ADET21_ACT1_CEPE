6cab6b4bd70b2d181eae6b9d789f5bf4363995c4
asbhycepe <ashby.cepe@unc.edu.ph>
2022-03-27 15:00:05 +0800

    Added function for getting and cheking user inputs

--- main.dart
+++ main.dart
@@ -7,5 +7,33 @@ void main(List<String> args) {
   print("- Current Balance: P 0     -");
   print("----------------------------");
   print("- [1] WITHDRAR  [2]DEPOSIT -");
+  print("- [3] EXIT                 -");
   print("----------------------------");
+
+  var choice = getChoice();
+}
+
+//Function for getting input
+int getChoice() {
+  var choice;
+
+  do {
+    stdout.write("Enter Operation: ");
+    choice = stdin.readLineSync();
+
+    if (checkInvalidInput(choice)) {
+      print("Not a valid option");
+    } else {
+      return int.parse(choice); //converts value into integer
+    }
+  } while (true);
+}
+
+//Function for checking invalid inputs
+bool checkInvalidInput(var choice) {
+  if (int.tryParse(choice ?? "") == null || int.parse(choice) > 3) {
+    return true;
+  } else {
+    return false;
+  }
 }
