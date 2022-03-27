6889ab1f362ee5d618397ac270cef26c518b0a8f
asbhycepe <ashby.cepe@unc.edu.ph>
2022-03-27 14:37:55 +0800

    FIRST COMMIT

---
+++ main.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+
+void main(List<String> args) {
+  print("----------------------------");
+  print("- Automated Teller Machine -");
+  print("----------------------------");
+  print("- Current Balance: P 0     -");
+  print("----------------------------");
+  print("- [1] WITHDRAR  [2]DEPOSIT -");
+  print("----------------------------");
+}
