d59d74594166dadc5d160d7b603d8108a98f8574
asbhycepe <ashby.cepe@unc.edu.ph>
2022-04-14 12:36:24 +0800

    Added new field for account type and its getters and setters methods

--- userAccount.dart
+++ userAccount.dart
@@ -3,21 +3,25 @@ import 'dart:io';
 class userAccount {
   String userName = "";
   String pin = "";
+  String accountType = "";
   int currentBal = 0;
 
-  userAccount(String userName, String pin, int currentBal) {
+  userAccount(String userName, String pin, int currentBal, String accountType) {
     this.setUserName(userName);
     this.setPin(pin);
     this.setCurrentBal(currentBal);
+    this.setAccountType(accountType);
   }
 
   void setUserName(String userName) => this.userName = userName;
   void setPin(String pin) => this.pin = pin;
   void setCurrentBal(int currentBal) => this.currentBal = currentBal;
+  void setAccountType(String accountType) => this.accountType = accountType;
 
   String getUserName() => this.userName;
   String getPin() => this.pin;
   int getCurrentBal() => this.currentBal;
+  String getAccountType() => this.accountType;
 
   // Fucntion for updatin the user account
   void updateUserAccount(userAccount account) {
